61e3ebcfb5981b8445c59b2b2b6e057abdeffd05
Ellie-Aghajani <z.aqajani@gmail.com>
2024-05-08 13:28:15 -0700

    schema

--- app.js
+++ app.js
@@ -2,4 +2,13 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/giftshop')
     .then(() => console.log("connected to mongodb ..."))
     .catch(err => console.log(err.message));
-    
+
+const giftSchema = new mongoose.Schema({
+    name: String,
+    category: [String],
+    description: String,
+    imageURL:String,
+    tags:[String],
+    createdAt:{ type: Date, default: Date. now }
+
+});
