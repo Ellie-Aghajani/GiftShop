98afc70163d510828deaef207b486e6810e8889e
Ellie-Aghajani <z.aqajani@gmail.com>
2024-05-08 17:10:27 -0700

    add gift schema

--- app.js
+++ app.js
@@ -12,3 +12,34 @@ const giftSchema = new mongoose.Schema({
     createdAt:{ type: Date, default: Date. now }
 
 });
+
+const Gift = mongoose.model('Gift', giftSchema);
+
+async function createGift(){
+    try{
+        const gift = new Gift({
+            name: "Mother's day box",
+            category: ['women'],
+            description: 'Bring joy to your mother with this box',
+            imageURL:String,
+            tags:["Mother's day"]
+        });
+        const result = await gift.save();
+        console.log(result);
+
+    }
+    catch(err){err => console.log('Erorr creating gift: ', err.message)};
+
+};
+// createGift();
+async function getGifts(){
+    return await Gift.find();
+
+}
+
+async function run(){
+    const gifts = await getGifts();
+    console.log(gifts)
+}
+
+run();
